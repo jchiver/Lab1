fd76bf748ded1e2338ff9e6144f3ce49d92a7bf6
jchiver <34653878+jchiver@users.noreply.github.com>
2017-12-18 14:54:50 +0000

    Update 1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Settings Demo: Linux Academy: Microsoft Azure Exam 70-533 Prep abc </title>
+		<title>Settings Demo: Linux Academy: Microsoft Azure Exam 70-533 Prep</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
@@ -10,7 +10,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h2>Settings Demo: Linux Academy: Microsoft Azure Exam 70-533 Prep</h2>
+					<h2>Settings Demo: Linux Academy: Microsoft Azure Exam 70-533 Prep - Using GitHub</h2>
 					<p>This PHP page shows the settings in this Azure Web App.</p>
 					<table class="table table-striped">
 						<thead>
